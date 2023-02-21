0d61d8a79069a5551adf7745e2f5be1b48ea9c77
artem <artem.burashnikov@gmail.com>
2023-02-22 00:23:47 +0300

    Fix input and output path Add amdDevice and amdContext

--- src/ImageProcessing/Main.fs
+++ src/ImageProcessing/Main.fs
@@ -3,51 +3,35 @@ namespace ImageProcessing
 open Brahma.FSharp
 
 module Main =
-    let pathToExamples = "/home/gsv/Projects/TestProj2020/src/ImgProcessing/Examples"
+    let pathToExamples = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "images")
     let inputFolder = System.IO.Path.Combine(pathToExamples, "input")
+    let outputFolder = System.IO.Path.Combine(pathToExamples, "output")
 
-    let demoFile =
-        System.IO.Path.Combine(inputFolder, "armin-djuhic-ohc29QXbS-s-unsplash.jpg")
 
     [<EntryPoint>]
     let main (argv: string array) =
-        let nvidiaDevice =
-            ClDevice.GetAvailableDevices(platform = Platform.Nvidia)
-            |> Seq.head
 
-        let intelDevice =
-            ClDevice.GetAvailableDevices(platform = Platform.Intel)
-            |> Seq.head
-        //ClDevice.GetFirstAppropriateDevice()
-        //printfn $"Device: %A{device.Name}"
+        (*
+        let amdDevice = ClDevice.GetAvailableDevices(platform = Platform.Amd) |> Seq.head
 
-        let nvContext = ClContext(nvidiaDevice)
-        let applyFiltersOnNvGPU = ImageProcessing.applyFiltersGPU nvContext 64
+        let amdContext = ClContext(amdDevice)
+        let applyFiltersOnAmdGPU = ImageProcessing.applyFiltersGPU amdContext 64
 
-        let intelContext = ClContext(intelDevice)
-        let applyFiltersOnIntelGPU = ImageProcessing.applyFiltersGPU intelContext 64
+        let filters =
+            [ ImageProcessing.gaussianBlurKernel
+              ImageProcessing.gaussianBlurKernel
+              ImageProcessing.edgesKernel ]
 
-        let filters = [
-            ImageProcessing.gaussianBlurKernel
-            ImageProcessing.gaussianBlurKernel
-            ImageProcessing.edgesKernel
-        ]
+        // let grayscaleImage = ImageProcessing.loadAs2DArray demoFile
+        // let blur = ImageProcessing.applyFilter ImageProcessing.gaussianBlurKernel grayscaleImage
+        // let edges = ImageProcessing.applyFilter ImageProcessing.edgesKernel blur
+        // let edges = applyFiltersGPU [ImageProcessing.gaussianBlurKernel; ImageProcessing.edgesKernel] grayscaleImage
+        // ImageProcessing.save2DByteArrayAsImage edges ""
 
-        //let grayscaleImage = ImageProcessing.loadAs2DArray demoFile
-        //let blur = ImageProcessing.applyFilter ImageProcessing.gaussianBlurKernel grayscaleImage
-        //let edges = ImageProcessing.applyFilter ImageProcessing.edgesKernel blur
-        //let edges =  applyFiltersGPU [ImageProcessing.gaussianBlurKernel; ImageProcessing.edgesKernel] grayscaleImage
-        //ImageProcessing.save2DByteArrayAsImage edges "../../../../../out/demo_grayscale.jpg"
         let start = System.DateTime.Now
 
-        Streaming.processAllFiles inputFolder "../../../../../out/" [
-            applyFiltersOnNvGPU filters
-            applyFiltersOnIntelGPU filters
-        ]
+        Streaming.processAllFiles inputFolder outputFolder [ applyFiltersOnAmdGPU filters ]
 
-        printfn
-            $"TotalTime = %f{(System.DateTime.Now
-                              - start)
-                                 .TotalMilliseconds}"
-
+        printfn $"TotalTime = %f{(System.DateTime.Now - start).TotalMilliseconds}"
+        *)
         0
