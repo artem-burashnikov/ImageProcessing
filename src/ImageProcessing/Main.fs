611672f914a9ca37b72823293ab5941562aa0a06
artem <artem.burashnikov@gmail.com>
2023-02-23 00:08:35 +0300

    Add InputPath, OutputPath types, set of image extensions

--- src/ImageProcessing/Main.fs
+++ src/ImageProcessing/Main.fs
@@ -1,14 +1,29 @@
 namespace ImageProcessing
 
 open Brahma.FSharp
+open Argu
 
 module Main =
-    let pathToExamples = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "images")
-    let inputFolder = System.IO.Path.Combine(pathToExamples, "input")
-    let outputFolder = System.IO.Path.Combine(pathToExamples, "output")
 
+    let extensions = set [| ".gif"; ".jpg"; ".jpeg"; ".bmp"; ".pbm"; ".png"; ".tiff"; ".tga"; ".webp" |]
+
+    [<RequireQualifiedAccess>]
+    type InputPath =
+        | File of string
+        | Folder of string
+        | Multiple of files: string list * folder: string list
+        | NoImgFile of string
+        | NotFound of string
+        | Unspecified
+
+    [<RequireQualifiedAccess>]
+    type OutputPath =
+        | IO of string
+        | NotKnown
+
+    let isImg (file: string) =
+        Set.contains (System.IO.Path.GetExtension file) extensions
 
     [<EntryPoint>]
     let main (argv: string array) =
-
         0
