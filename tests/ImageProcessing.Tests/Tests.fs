e1f7cb86b948b706abb4f266be014686eca1c065
artem <artem.burashnikov@gmail.com>
2023-02-25 22:25:51 +0300

    Add 2DArrays rotation property tests

--- tests/ImageProcessing.Tests/Tests.fs
+++ tests/ImageProcessing.Tests/Tests.fs
@@ -2,12 +2,50 @@ namespace ImageProcessing.Tests
 
 open Expecto
 open ImageProcessing
+open Microsoft.VisualStudio.TestPlatform.ObjectModel
 
-module SayTests =
+module TestSamples =
+
+    let src = __SOURCE_DIRECTORY__
+    let testSamplesFolder = "TestSamples"
+    let imagesFolder = "Images"
+    let inputFolder = "Input"
+    let outputFolder = "Output"
+    let inputImg = "sample.jpg"
+    let outputImg = "out_sample.jpg"
+    let fp = System.IO.Path.Combine(testSamplesFolder, imagesFolder)
+
+    let r = System.Random()
+
     [<Tests>]
     let tests =
         testList
             "samples"
             [
+            testProperty "Rotating clockwise 4 times outputs the original image"
+            <| fun (width: uint) (height: uint) ->
+               // "+2" because minimum testing for 2x2 tables
+               let w, h = System.Convert.ToInt32 width + 2, System.Convert.ToInt32 height + 2
+               let originalImg = Array2D.init w h (fun _ _ -> byte (r.Next(0,256)))
+               let rotate = ImageProcessing.rotate90Clockwise
+               let rotatedImg = rotate originalImg |> rotate |> rotate |> rotate
+               Expect.equal rotatedImg originalImg "Clockwise rotated 4 times failed to match the original image"
+
+            testProperty "Rotating counterclockwise 4 times outputs the original image"
+            <| fun (width: uint) (height: uint) ->
+               // "+2" because minimum testing for 2x2 tables
+               let w, h = System.Convert.ToInt32 width + 2, System.Convert.ToInt32 height + 2
+               let originalImg = Array2D.init w h (fun _ _ -> byte (r.Next(0,256)))
+               let rotate = ImageProcessing.rotate90Counterclockwise
+               let rotatedImg = rotate originalImg |> rotate |> rotate |> rotate
+               Expect.equal rotatedImg originalImg "Counterclockwise rotated 4 times failed to match the original image"
+
+            testProperty "Rotating clockwise then counterclockwise outputs the original image"
+            <| fun (width: uint) (height: uint) ->
+               // "+2" because minimum testing for 2x2 tables
+               let w, h = System.Convert.ToInt32 width + 2, System.Convert.ToInt32 height + 2
+               let originalImg = Array2D.init w h (fun _ _ -> byte (r.Next(0,256)))
+               let rotatedImg = ImageProcessing.rotate90Clockwise originalImg |> ImageProcessing.rotate90Counterclockwise
+               Expect.equal rotatedImg originalImg "Clockwise and then counterclockwise failed to match the original image"
 
             ]
