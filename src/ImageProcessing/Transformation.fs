182cd63ec7508bddfcd2c386813b058080fa354f
artem <artem.burashnikov@gmail.com>
2023-03-19 20:48:24 +0300

    feat: unify cpu and gpu transformatin application

--- src/ImageProcessing/Transformation.fs
+++ src/ImageProcessing/Transformation.fs
@@ -1,6 +1,7 @@
 module ImageProcessing.Transformation
 
 open ImageProcessing.ImageProcessing
+open Brahma.FSharp
 
 [<RequireQualifiedAccess>]
 type Transformation =
@@ -25,14 +26,48 @@ type Transformation =
            ReflectH
            ReflectV |]
 
-let getTsf applicatorFunction (transformation: Transformation) =
+let getTsfCPU transformation =
     match transformation with
-    | Transformation.Blur -> applicatorFunction gaussianBlurKernel
-    | Transformation.Edges -> applicatorFunction edgesKernel
-    | Transformation.HighPass -> applicatorFunction highPassKernel
-    | Transformation.Laplacian -> applicatorFunction laplacianKernel
-    | Transformation.SobelV -> applicatorFunction sobelVerticalKernel
-    | Transformation.Rotate -> rotate90Clockwise
-    | Transformation.RotateCCW -> rotate90Counterclockwise
-    | Transformation.ReflectH -> horizontalReflect
-    | Transformation.ReflectV -> verticalReflect
+    | Transformation.Blur -> applyFilterCPU gaussianBlurKernel
+    | Transformation.Edges -> applyFilterCPU edgesKernel
+    | Transformation.HighPass -> applyFilterCPU highPassKernel
+    | Transformation.Laplacian -> applyFilterCPU laplacianKernel
+    | Transformation.SobelV -> applyFilterCPU sobelVerticalKernel
+    | Transformation.Rotate -> rotateCPU Clockwise
+    | Transformation.RotateCCW -> rotateCPU Counterclockwise
+    | Transformation.ReflectH -> reflectCPU Horizontal
+    | Transformation.ReflectV -> reflectCPU Vertical
+
+let getTsfGPU (clContext: ClContext) localWorkSize transformation : (Image -> Image) =
+
+    let filterKernel = Kernel<_>.makeFilterKernel clContext localWorkSize
+
+    match transformation with
+    | Transformation.Blur -> applyFilterGPU filterKernel clContext gaussianBlurKernel
+    | Transformation.Edges -> applyFilterGPU filterKernel clContext edgesKernel
+    | Transformation.HighPass -> applyFilterGPU filterKernel clContext highPassKernel
+    | Transformation.Laplacian -> applyFilterGPU filterKernel clContext laplacianKernel
+    | Transformation.SobelV -> applyFilterGPU filterKernel clContext sobelVerticalKernel
+
+    | Transformation.Rotate ->
+        let rotationKernel = Kernel<_>.makeRotationKernel clContext localWorkSize Clockwise
+
+        rawProcessGPU rotationKernel clContext
+
+    | Transformation.RotateCCW ->
+        let rotationKernel =
+            Kernel<_>.makeRotationKernel clContext localWorkSize Counterclockwise
+
+        rawProcessGPU rotationKernel clContext
+
+    | Transformation.ReflectH ->
+        let reflectionKernel =
+            Kernel<_>.makeReflectionKernel clContext localWorkSize Horizontal
+
+        rawProcessGPU reflectionKernel clContext
+
+    | Transformation.ReflectV ->
+        let reflectionKernel =
+            Kernel<_>.makeReflectionKernel clContext localWorkSize Vertical
+
+        rawProcessGPU reflectionKernel clContext
