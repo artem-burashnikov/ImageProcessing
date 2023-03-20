751fcb1ba9ade74185d0d6f0fe0849a2be64113c
artem <artem.burashnikov@gmail.com>
2023-03-21 01:28:36 +0300

    add: gpu transformation parser

--- src/ImageProcessing/Transformation.fs
+++ src/ImageProcessing/Transformation.fs
@@ -1,6 +1,7 @@
 module ImageProcessing.Transformation
 
 open ImageProcessing.ImageProcessing
+open Brahma.FSharp
 
 [<RequireQualifiedAccess>]
 type Transformation =
@@ -36,3 +37,20 @@ let getTsfCPU transformation =
     | Transformation.RotateCCW -> rotateCPU Counterclockwise
     | Transformation.ReflectH -> reflectCPU Horizontal
     | Transformation.ReflectV -> reflectCPU Vertical
+
+let getTsfGPU (clContext: ClContext) localWorkSize transformation =
+
+    let filterKernel = lazy Kernel<_>.makeFilterKernel clContext localWorkSize
+    let rotationKernel = lazy Kernel<_>.makeRotationKernel clContext localWorkSize
+    let reflectionKernel = lazy Kernel<_>.makeReflectionKernel clContext localWorkSize
+
+    match transformation with
+    | Transformation.Blur -> applyFilterGPU (filterKernel.Force()) clContext gaussianBlurKernel
+    | Transformation.Edges -> applyFilterGPU (filterKernel.Force()) clContext edgesKernel
+    | Transformation.HighPass -> applyFilterGPU (filterKernel.Force()) clContext highPassKernel
+    | Transformation.Laplacian -> applyFilterGPU (filterKernel.Force()) clContext laplacianKernel
+    | Transformation.SobelV -> applyFilterGPU (filterKernel.Force()) clContext sobelVerticalKernel
+    | Transformation.Rotate -> rawProcessGPU (rotationKernel.Force () Clockwise) clContext
+    | Transformation.RotateCCW -> rawProcessGPU (rotationKernel.Force () Counterclockwise) clContext
+    | Transformation.ReflectH -> rawProcessGPU (reflectionKernel.Force () Horizontal) clContext
+    | Transformation.ReflectV -> rawProcessGPU (reflectionKernel.Force () Vertical) clContext
