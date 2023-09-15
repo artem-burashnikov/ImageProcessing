bf6e9d87d34af17fafcd1c01c22d77332445fe29
artem-burashnikov <artem.burashnikov@gmail.com>
2023-09-16 00:45:02 +0300

    add: xml commentary

--- src/ImageProcessing/Logging.fs
+++ src/ImageProcessing/Logging.fs
@@ -1,13 +1,32 @@
+/// <summary>
+/// Provides logging functionality for image processing operations.
+/// </summary>
 module ImageProcessing.Logging
 
+/// <summary>
+/// Gets the current time as a formatted string.
+/// </summary>
+/// <returns>A formatted string representing the current time.</returns>
 let getTime () =
     let now = System.DateTime.Now
     sprintf $"%02d{now.Hour}:%02d{now.Minute}:%02d{now.Second}:%03d{now.Millisecond}"
 
+/// <summary>
+/// Represents control messages for the logger agent.
+/// </summary>
 type LogControl =
+    /// <summary>
+    /// Represents a log message.
+    /// </summary>
     | Message of string
+    /// <summary>
+    /// Represents a control message to stop the logger agent.
+    /// </summary>
     | Stop
 
+/// <summary>
+/// Provides logging functionality.
+/// </summary>
 type Logger() =
     let mutable agentIsRunning = true
 
@@ -23,6 +42,18 @@ type Logger() =
 
     let logger = new MailboxProcessor<LogControl>(agent)
 
+    /// <summary>
+    /// Starts the logger agent.
+    /// </summary>
     member _.Start() = logger.Start()
+
+    /// <summary>
+    /// Stops the logger agent gracefully.
+    /// </summary>
     member _.Stop() = logger.Post(Stop)
-    member _.Log msg = logger.Post(Message msg)
+
+    /// <summary>
+    /// Logs a message.
+    /// </summary>
+    /// <param name="msg">The message to log.</param>
+    member _.Log(msg: string) = logger.Post(Message msg)
